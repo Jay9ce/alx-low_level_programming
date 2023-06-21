f257634651d3ec95d733f6c5f8bf7722f2a5c350
Jay9ce <joshuaichipi@gmail.com>
2023-06-21 11:28:52 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,3 +30,18 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(result + '0');
 				} else if (result < 100)
+				{
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				} else
+				{
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
